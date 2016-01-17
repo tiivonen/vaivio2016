6fca4e02abf857e48aee1e2c61894274741fcf0e
Timo Iivonen <timo.iivonen@gmail.com>
2016-01-18 01:48:23 +0200

    added imagemin and ignored html folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
-var partialimport  = require('postcss-partial-import');
+var partialimport = require('postcss-partial-import');
 var csswring = require('csswring');
 var cssnext = require('postcss-cssnext');
 var autoprefixer = require('autoprefixer');
@@ -15,6 +15,8 @@ var grace = require('cssgrace');
 var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var jade = require('gulp-jade');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 
 
@@ -30,7 +32,9 @@ gulp.task('styles', function() {
     nested(),
     cssnext({}),
 
-    autoprefixer({browsers:['last 2 versions']}),
+    autoprefixer({
+      browsers: ['last 2 versions']
+    }),
     // csswring({
     //   removeAllComments: true
     // })
@@ -40,17 +44,34 @@ gulp.task('styles', function() {
   return gulp.src('./dev/assets/css/styles.css')
     // .pipe( sourcemaps.init())
     .pipe(plumber({
-      errorHandler: function (error) {
+      errorHandler: function(error) {
         gutil.log(gutil.colors.yellow(error.message));
         gutil.beep();
         this.emit('end');
-    }}))
+      }
+    }))
     .pipe(postcss(processors))
     // .pipe( sourcemaps.write('.'))
     .pipe(gulp.dest('./html/assets/css'))
-    .pipe(browsersync.reload({stream:true}));
+    .pipe(browsersync.reload({
+      stream: true
+    }));
 });
 
+gulp.task('imagemin', function() {
+  gulp.src('./dev/assets/img/**/*.{gif,jpg,png,svg}')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('./html/assets/img'))
+    .pipe(browsersync.reload({
+      stream: true
+    }));;
+});
 
 gulp.task('jade', function() {
   gulp.src('./dev/*.jade')
@@ -58,33 +79,36 @@ gulp.task('jade', function() {
       pretty: true
     }))
     .pipe(gulp.dest('./html'))
-    .pipe(browsersync.reload({stream:true}));
+    .pipe(browsersync.reload({
+      stream: true
+    }));
 
 });
 
 gulp.task('browsersync', ['styles', 'jade'], function() {
-    browsersync({
-        browser: "google chrome",
-        server: {
-            baseDir: './html'
-        }
-    });
-    gulp.watch("*.html").on("change", browsersync.reload);
+  browsersync({
+    browser: "google chrome",
+    server: {
+      baseDir: './html'
+    }
+  });
+  gulp.watch("*.html").on("change", browsersync.reload);
 });
 
 // gulp.task('watch:styles' function(){
 //   gulp.watch('**/*.css', ['styles']);
 // });
 
-gulp.task('watch', function () {
-    gulp.watch('dev/assets/css/*.css', ['styles']);
-    // gulp.watch('assets/js/**', ['minify']);
-    // gulp.watch(['index.html', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
-    gulp.watch(['dev/*.jade'], ['jade']);
+gulp.task('watch', function() {
+  gulp.watch('dev/assets/css/*.css', ['styles']);
+  // gulp.watch('assets/js/**', ['minify']);
+  // gulp.watch(['index.html', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
+  gulp.watch(['dev/*.jade'], ['jade']);
+  gulp.watch(['dev/assets/img/**/*.{gif,jpg,png,svg}'], ['imagemin']);
 });
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browsersync', 'watch', 'styles', 'jade']);
+gulp.task('default', ['browsersync', 'watch', 'styles', 'jade', 'imagemin']);
